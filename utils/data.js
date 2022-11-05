8946c7cd72ee5279fa71864e983e9cae7fab4256
kaihuan Huang <huanhuan@kaihuans-MacBook-Pro.local>
2022-11-04 17:10:54 -0700

    await <name-of-model>.collection.drop(); fixed dub key email_1 error

--- utils/data.js
+++ utils/data.js
@@ -1,117 +1,117 @@
-// const userNames = [
-//   'Aaran',
-//   'Aaren',
-//   'Aarez',
-//   'Aarman',
-//   'Aaron',
-//   'Aaron-James',
-//   'Aarron',
-//   'Aaryan',
-//   'Aaryn',
-//   'Aayan',
-//   'Aazaan',
-//   'Abaan',
-//   'Abbas',
-//   'Abdallah',
-//   'Abdalroof',
-//   'Abdihakim',
-//   'Abdirahman',
-//   'Abdisalam',
-//   'Abdul',
-//   'Abdul-Aziz',
-//   'Abdulbasir',
-//   'Abdulkadir',
-//   'Abdulkarem',
-//   'Smith',
-//   'Jones',
-//   'Coollastname',
-//   'enter_name_here',
-//   'Ze',
-//   'Zechariah',
-//   'Zeek',
-//   'Zeeshan',
-//   'Zeid',
-//   'Zein',
-//   'Zen',
-//   'Zendel',
-//   'Zenith',
-//   'Zennon',
-//   'Zeph',
-//   'Zerah',
-//   'Zhen',
-//   'Zhi',
-//   'Zhong',
-//   'Zhuo',
-//   'Zi',
-//   'Zidane',
-//   'Zijie',
-//   'Zinedine',
-//   'Zion',
-//   'Zishan',
-//   'Ziya',
-//   'Ziyaan',
-//   'Zohaib',
-//   'Zohair',
-//   'Zoubaeir',
-//   'Zubair',
-//   'Zubayr',
-//   'Zuriel',
-//   'Xander',
-//   'Jared',
-//   'Courtney',
-//   'Gillian',
-//   'Clark',
-//   'Jared',
-//   'Grace',
-//   'Kelsey',
-//   'Tamar',
-//   'Alex',
-//   'Mark',
-//   'Tamar',
-//   'Farish',
-//   'Sarah',
-//   'Nathaniel',
-//   'Parker',
-// ];
+const userNames = [
+  'Aaran',
+  'Aaren',
+  'Aarez',
+  'Aarman',
+  'Aaron',
+  'Aaron-James',
+  'Aarron',
+  'Aaryan',
+  'Aaryn',
+  'Aayan',
+  'Aazaan',
+  'Abaan',
+  'Abbas',
+  'Abdallah',
+  'Abdalroof',
+  'Abdihakim',
+  'Abdirahman',
+  'Abdisalam',
+  'Abdul',
+  'Abdul-Aziz',
+  'Abdulbasir',
+  'Abdulkadir',
+  'Abdulkarem',
+  'Smith',
+  'Jones',
+  'Coollastname',
+  'enter_name_here',
+  'Ze',
+  'Zechariah',
+  'Zeek',
+  'Zeeshan',
+  'Zeid',
+  'Zein',
+  'Zen',
+  'Zendel',
+  'Zenith',
+  'Zennon',
+  'Zeph',
+  'Zerah',
+  'Zhen',
+  'Zhi',
+  'Zhong',
+  'Zhuo',
+  'Zi',
+  'Zidane',
+  'Zijie',
+  'Zinedine',
+  'Zion',
+  'Zishan',
+  'Ziya',
+  'Ziyaan',
+  'Zohaib',
+  'Zohair',
+  'Zoubaeir',
+  'Zubair',
+  'Zubayr',
+  'Zuriel',
+  'Xander',
+  'Jared',
+  'Courtney',
+  'Gillian',
+  'Clark',
+  'Jared',
+  'Grace',
+  'Kelsey',
+  'Tamar',
+  'Alex',
+  'Mark',
+  'Tamar',
+  'Farish',
+  'Sarah',
+  'Nathaniel',
+  'Parker',
+];
 
-// const thoughts = [
-//   'Decision Tracker',
-//   'Find My Phone',
-//   'Learn Piano',
-//   'Starbase Defender',
-//   'Tower Defense',
-//   'Monopoly Money Manager',
-//   'Movie trailers',
-//   'Hello world',
-//   'Stupid Social Media App',
-//   'Notes',
-//   'Messages',
-//   'Email',
-//   'Compass',
-//   'Firefox',
-//   'Running app',
-//   'Cooking app',
-//   'Poker',
-//   'Deliveries',
-// ];
+const thoughts = [
+  'Decision Tracker',
+  'Find My Phone',
+  'Learn Piano',
+  'Starbase Defender',
+  'Tower Defense',
+  'Monopoly Money Manager',
+  'Movie trailers',
+  'Hello world',
+  'Stupid Social Media App',
+  'Notes',
+  'Messages',
+  'Email',
+  'Compass',
+  'Firefox',
+  'Running app',
+  'Cooking app',
+  'Poker',
+  'Deliveries',
+];
 
-// // Get a random item given an array
-// const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+// Get a random item given an array
+const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
-// // Gets a random full name
-// const getRandomName = () =>
-//   `${getRandomArrItem(userNames)} ${getRandomArrItem(userNames)}`;
+// Gets a random full name
+const getRandomName = () =>
+  `${getRandomArrItem(userNames)} ${getRandomArrItem(userNames)}`;
 
-// // Function to generate random thoughts that we can add to user object.
-// const getRandomThoughts = (int) => {
-//   const results = [];
-//   for (let i = 0; i < int; i++) {
-//     results.push({
-//       thoughts: getRandomArrItem(thoughts),
-//     });
-//   }
-//   return results;
-// };
+// Function to generate random thoughts that we can add to user object.
+const getRandomThoughts = (int) => {
+  const results = [];
+  for (let i = 0; i < int; i++) {
+    results.push({
+      thoughts: getRandomArrItem(thoughts),
+    });
+  }
+  return results;
+};
 
-// // Export the functions for use in seed.js
-// module.exports = { getRandomName, getRandomThoughts };
+// Export the functions for use in seed.js
+module.exports = { getRandomName, getRandomThoughts };
--- utils/seed.js
+++ utils/seed.js
@@ -1,46 +1,46 @@
-// const connection = require('../config/connection');
-// const { Thought, User } = require('../models');
-// const { getRandomThoughts, getRandomName} = require('./data');
+const connection = require('../config/connection');
+const { Thought, User } = require('../models');
+const { getRandomThoughts, getRandomName} = require('./data');
 
-// connection.on('error', (err) => err);
+connection.on('error', (err) => err);
 
-// connection.once('open', async () => {
-//   console.log('connected');
+connection.once('open', async () => {
+  console.log('connected');
 
-//   // Drop existing thoughts
-//   await Thought.deleteMany({});
+  // Drop existing thoughts
+  await Thought.collection.drop();
 
-//   // Drop existing users
-//   await User.deleteMany({});
+  // Drop existing users
+  await User.collection.drop();
 
-//   // Create empty array to hold the users
-//   const users = [];
+  // Create empty array to hold the users
+  const users = [];
 
-//   // Loop 20 times -- add students to the users array
-//   for (let i = 0; i < 20; i++) {
-//     // Get some random thought objects using a helper function that we imported from ./data
-//     const thoughts = getRandomThoughts(20);
-//     const userNames = getRandomName(); //return string
+  // Loop 20 times -- add students to the users array
+  for (let i = 0; i < 20; i++) {
+    // Get some random thought objects using a helper function that we imported from ./data
+    const thoughts = getRandomThoughts(20);
+    const userNames = getRandomName(); //return string
 
-//     users.push({
-//       userNames,//data.js
-//       thoughts
-//     });
-//   }
+    users.push({
+      userNames,//data.js
+      thoughts
+    });
+  }
 
-//   // Add users to the collection and await the results
-//   await User.collection.insertMany(users);
+  // Add users to the collection and await the results
+  await User.collection.insertMany(users);
 
-//   // Add thoughts to the collection and await the results
-//   await Thought.collection.insertOne({
-//     thoughtText: 'I feel great!',
-//     email: [...email],
-//     users: [...users],
-//     reaction: [...reactions]
-//   });
+  // Add thoughts to the collection and await the results
+  await Thought.collection.insertOne({
+    thoughtText: 'I feel great!',
+    email: [...email],
+    users: [...users],
+    reaction: [...reactions]
+  });
 
-//   // Log out the seed data to indicate what should appear in the database
-//   console.table(users);
-//   console.info('Seeding complete! 🌱');
-//   process.exit(0);
-// });
+  // Log out the seed data to indicate what should appear in the database
+  console.table(users);
+  console.info('Seeding complete! 🌱');
+  process.exit(0);
+});

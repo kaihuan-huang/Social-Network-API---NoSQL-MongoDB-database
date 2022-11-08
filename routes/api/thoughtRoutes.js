8a8f2e80c3eb60e280bb98459ebee788054f8317
kaihuan Huang <huanhuan@kaihuans-MacBook-Pro.local>
2022-11-07 18:25:16 -0800

    thought space bug fixed

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -23,7 +23,7 @@ router.route('/:userId').post(createThought);
 router.route('/:thoughtId/reactions').post(addReaction);
 
 // //api/thoughts/:thoughtId/reactions/reactionId
-router.route('/:thoughtId/reactions/:reactionId ').delete(deleteReaction);
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
   
 
 module.exports = router;
